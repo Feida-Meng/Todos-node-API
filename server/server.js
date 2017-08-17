54b5ca8bc569ea51b28f7986b6a3b8ecdf33a64f
codonculous <feidameng01@gmail.com>
2017-08-17 12:00:34 -0400

    Created User mongoose model with validations, and tested validationos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,39 @@ var Todo = mongoose.model('Todo', {
   }
 });
 
+
+var User = mongoose.model('User',{
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5
+  }
+});
+
+//tests should fail
+// var yingzheng = new User({});
+
+// var yingzheng = new User({
+//   email: 'sf'
+// });
+//
+// var yingzheng = new User({
+//   email: false
+// });
+
+//tests should pass
+// var yingzheng = new User({
+//   email: 'yingzheng@qin.com'
+// });
+//
+yingzheng.save().then((doc) => {
+  console.log('User saved', doc);
+},(err) => {
+  console.log('Unable to save user, ', err);
+});
+
+
 // var studyMongodb = new Todo({
 //   text: 'Study Mongodb'
 // });
@@ -44,12 +77,12 @@ var Todo = mongoose.model('Todo', {
 //   console.log('Unable to save todo');
 // });
 
-var newTodo = new Todo({
-  text: ' Go to Mars  '
-});
-
-newTodo.save().then((doc) => {
-  console.log('Todo saved,', doc);
-},(err) => {
-  console.log('Unable to save todo', err.ValidationError);
-})
+// var newTodo = new Todo({
+//   text: ' Go to Mars  '
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Todo saved,', doc);
+// },(err) => {
+//   console.log('Unable to save todo', err.ValidationError);
+// })

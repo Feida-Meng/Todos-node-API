e1c9ca58a31b007880a33c995a9644d27f5521ad
codonculous <feidameng01@gmail.com>
2017-08-17 12:59:52 -0400

    Created route and response to post todo

--- server/server.js
+++ server/server.js
@@ -7,6 +7,22 @@ var { User } = require('./models/user');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.post('/todos/new',(req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
+
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });

5e579084f486e1f0d75c580e62a641160a0c486d
codonculous <feidameng01@gmail.com>
2017-08-17 11:28:32 -0400

    Created and saved two todo docs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
+});
 
 var Todo = mongoose.model('Todo', {
   text: {
@@ -11,6 +13,29 @@ var Todo = mongoose.model('Todo', {
     type: Boolean
   },
   completedAt: {
-    type:number
+    type: Number
   }
+});
+
+var studyMongodb = new Todo({
+  text: 'Study Mongodb'
+});
+
+studyMongodb.save().then((doc) => {
+  console.log('Saved todo',doc);
+},(err) => {
+  console.log('Unable to save todo');
+});
+
+var studyNode = new Todo({
+  text: 'Study Node',
+  completed: false,
+  completedAt: 1
+
+});
+
+studyNode.save().then((doc) => {
+  console.log('Todo saved, ', doc);
+},(err) => {
+  console.log('Unable to save todo');
 });

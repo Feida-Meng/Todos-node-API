d71ad919a75cb279a761011ba1a4845ffac83724
codonculous <feidameng01@gmail.com>
2017-08-17 11:47:34 -0400

    Added validations to Todo modal

--- server/server.js
+++ server/server.js
@@ -7,35 +7,49 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', {
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-var studyMongodb = new Todo({
-  text: 'Study Mongodb'
+// var studyMongodb = new Todo({
+//   text: 'Study Mongodb'
+// });
+//
+// studyMongodb.save().then((doc) => {
+//   console.log('Saved todo',doc);
+// },(err) => {
+//   console.log('Unable to save todo');
+// });
+
+// var studyNode = new Todo({
+//   text: 'Study Node',
+//   completed: false,
+//   completedAt: 1
+//
+// });
+//
+// studyNode.save().then((doc) => {
+//   console.log('Todo saved, ', doc);
+// },(err) => {
+//   console.log('Unable to save todo');
+// });
+
+var newTodo = new Todo({
+  text: ' Go to Mars  '
 });
 
-studyMongodb.save().then((doc) => {
-  console.log('Saved todo',doc);
+newTodo.save().then((doc) => {
+  console.log('Todo saved,', doc);
 },(err) => {
-  console.log('Unable to save todo');
-});
-
-var studyNode = new Todo({
-  text: 'Study Node',
-  completed: false,
-  completedAt: 1
-
-});
-
-studyNode.save().then((doc) => {
-  console.log('Todo saved, ', doc);
-},(err) => {
-  console.log('Unable to save todo');
-});
+  console.log('Unable to save todo', err.ValidationError);
+})
